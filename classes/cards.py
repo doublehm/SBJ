2d9d761d297874603dc9dfa14d1061b05f636a0d
hossein <double.hm89@gmail.com>
2020-03-26 20:03:51 +0430

    updating few things

---
+++ classes/cards.py
@@ -0,0 +1,14 @@
+import random
+
+
+class Cards:
+
+    def __init__(self, cards, values):
+        self.values = values
+        self.names = [{"name": "A", "value": [1, 11]}, {"name": "2", "value": 2}, {"name": "3", "value": 3}, {"name": "4", "value": 4}
+        , {"name": "5", "value": 5}, {"name": "6", "value": 6}, {"name": "7", "value": 7}, {"name": "8", "value": 8}
+        , {"name": "9", "value": 9}, {"name": "10", "value": 10}, {"name": "jack", "value": 10}
+        , {"name": "queen", "value": 10}, {"name": "king", "value": 10}]
+
+
+
---
+++ classes/person.py
@@ -0,0 +1,8 @@
+class Person:
+    def __init__(self, cards_holding, value_sum, points):
+
+        self.value_sum = value_sum
+        self.points = points
+        self.cards_holding = cards_holding
+
+
