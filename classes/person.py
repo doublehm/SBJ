b3d956a8eef16d13fac9c0947eba4a9af1ed0fd1
Hossein Hedayati <double.hm89@gmail.com>
2020-03-26 23:07:36 +0430

    some small changes to classes

--- classes/person.py
+++ classes/person.py
@@ -1,8 +1,38 @@
-class Person:
-    def __init__(self, cards_holding, value_sum, points):
+import random
 
+class Person:
+    def __init__(self, name, cards_holding, value_sum, points):
+
+        self.name = name
         self.value_sum = value_sum
         self.points = points
         self.cards_holding = cards_holding
 
+    def dealer_first_turn_generator(self, cards):
+        dealers_first_card = self.cardgenerator(cards)
+        dealers_second_card = self.cardgenerator(cards)
+        self.cards_holding = dealers_first_card["name"] + " - " + dealers_second_card["name"]
+        self.value_sum = dealers_first_card["value"] + dealers_second_card["value"]
 
+    def player_first_turn_generator(self, cards):
+        players_first_card = self.cardgenerator(cards)
+        players_second_card = self.cardgenerator(cards)
+        self.cards_holding = players_first_card["name"] + " - " + players_second_card["name"]
+        self.value_sum = players_first_card["value"] + players_second_card["value"]
+        print(self.name + " is holding", self.cards_holding)
+        print(self.name, "cards value:", self.value_sum)
+
+    def card_holding_manager(self, item):
+        self.cards_holding += " - " + item["name"]
+        print(self.name + " is holding", self.cards_holding)
+
+    def cardgenerator(self, cards):
+
+        i = random.randrange(1, len(cards) + 1)
+        generated_card = cards[i]
+        del cards[i]
+        return generated_card
+
+    def card_value_manager(self, item):
+        self.value_sum += item["value"]
+        print(self.name, "cards value:", self.value_sum)
--- main.py
+++ main.py
@@ -18,16 +18,8 @@ while i < 52:
             i += 1
 
 
-def cardgenerator(cards):
-
-    i = random.randrange(1, len(cards) + 1)
-    generated_card = cards[i]
-    del cards[i]
-    return generated_card
-
-
-player = Person("", 0, 0)
-dealer = Person("", 0, 0)
+player = Person("player", "", 0, 0)
+dealer = Person("dealer", "", 0, 0)
 
 print("this is the game of blackjack")
 wanna_play = input("do you wanna proceed? (y/n): ")
@@ -44,28 +36,9 @@ while running:
     if wanna_play == "y":
         print("great lets start")
 
-        random_card = cardgenerator(cards)
+        dealer.dealer_first_turn_generator(cards)
+        player.player_first_turn_generator(cards)
         print("cards count: " + str(len(cards)))
-        dealer.cards_holding += random_card["name"]
-        dealer.value_sum += random_card["value"]
-
-        random_card = cardgenerator(cards)
-        print("cards count: " + str(len(cards)))
-        dealer.cards_holding += random_card["name"]
-        dealer.value_sum += random_card["value"]
-
-        random_card = cardgenerator(cards)
-        print("cards count: " + str(len(cards)))
-        player.cards_holding += random_card["name"]
-        player.value_sum += random_card["value"]
-
-        random_card = cardgenerator(cards)
-        print("cards count: " + str(len(cards)))
-        player.cards_holding += random_card["name"]
-        player.value_sum += random_card["value"]
-        print("your card is: " + player.cards_holding)
-
-        print("player's hand value: ", str(player.value_sum))
 
         x = input("you want another card? (y/n): ")
         if x == "n":
@@ -74,11 +47,10 @@ while running:
         if x == "y":
             while not dealers_turn:
 
-                player_extra_card = cardgenerator(cards)
+                player_extra_card = player.cardgenerator(cards)
                 print("cards count: " + str(len(cards)))
-                print("your last card is: " + player_extra_card['name'])
-                player.value_sum += player_extra_card["value"]
-                print("your hands value is:", player.value_sum)
+                player.card_holding_manager(player_extra_card)
+                player.card_value_manager(player_extra_card)
                 if player.value_sum > 21:
                     print("you lost")
                     break
@@ -96,25 +68,23 @@ while running:
             if player.value_sum < dealer.value_sum < 21:
                 print("dealer won! you lost")
                 break
-            elif dealer.value_sum > 21:
-                print("dealer lost! you won")
+            elif player.value_sum > 21:
+                print("you won!!")
                 break
 
             while dealer.value_sum <= player.value_sum:
 
-                dealers_card_extra = cardgenerator(cards)
+                dealers_card_extra = dealer.cardgenerator(cards)
                 print("cards count: " + str(len(cards)))
-                dealer.value_sum += dealers_card_extra["value"]
-                dealer.cards_holding += dealers_card_extra["name"]
-                print("dealers cards: " + dealer.cards_holding)
-                print("dealers card value: " + str(dealer.value_sum))
+                dealer.card_holding_manager(dealers_card_extra)
+                dealer.card_value_manager(dealers_card_extra)
 
                 if 21 > dealer.value_sum > player.value_sum:
                     print("dealer won!")
                     break
-                if dealer.value_sum > 21:
-                    print("you won")
-
+                elif dealer.value_sum > 21:
+                    print("you won!!")
+            break
     elif wanna_play == "n":
         print("exiting game")
         break
