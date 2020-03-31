93438bb10f1cebb1d6fab107a61191d5458ca175
Hossein Hedayati <double.hm89@gmail.com>
2020-03-31 11:32:03 +0430

    shuffling with errors

--- classes/person.py
+++ classes/person.py
@@ -1,5 +1,7 @@
 import random
 from classes.cards import Cards
+
+
 class Person:
     def __init__(self, name, cards_holding, value_sum, money):
 
@@ -8,33 +10,40 @@ class Person:
         self.money = money
         self.cards_holding = cards_holding
 
-    def dealer_first_turn_generator(self, cards, running):
-        dealers_first_card = self.cardgenerator(cards, running)
-        dealers_second_card = self.cardgenerator(cards, running)
+    def dealer_first_turn_generator(self, cards, running, name, kind):
+        dealers_first_card = self.cardgenerator(cards, running, name, kind)
+        dealers_second_card = self.cardgenerator(cards, running, name, kind)
+
         self.cards_holding = dealers_first_card["name"] + " - " + dealers_second_card["name"]
-    #    self.value_sum = self.card_value_manager(dealers_first_card) + self.card_value_manager(dealers_second_card)
-    #    self.value_sum = dealers_first_card["value"] + dealers_second_card["value"]
+
         if dealers_first_card["value"] == [1, 11]:
             self.value_sum = dealers_first_card["value"][1] + dealers_second_card["value"]
+
         elif dealers_second_card["value"] == [1, 11]:
             self.value_sum = dealers_first_card["value"] + dealers_second_card["value"][1]
+
         elif dealers_first_card["value"] ==[1, 11] and dealers_second_card["value"] == [1, 11]:
             self.value_sum = 21
+
         else:
             self.value_sum = dealers_first_card["value"] + dealers_second_card["value"]
 
-    def player_first_turn_generator(self, cards, running):
-        players_first_card = self.cardgenerator(cards, running)
-        players_second_card = self.cardgenerator(cards, running)
+    def player_first_turn_generator(self, cards, running, name, kind):
+
+        players_first_card = self.cardgenerator(cards, running, name, kind)
+        players_second_card = self.cardgenerator(cards, running, name, kind)
+
         self.cards_holding = players_first_card["name"] + " - " + players_second_card["name"]
-    #    self.value_sum = self.card_value_manager(players_first_card) + self.card_value_manager(players_second_card)
-#        self.value_sum = players_first_card["value"] + players_second_card["value"]
+
         if players_first_card["value"] == [1, 11]:
             self.value_sum = players_first_card["value"][1] + players_second_card["value"]
+
         elif players_second_card["value"] == [1, 11]:
             self.value_sum = players_first_card["value"] + players_second_card["value"][1]
+
         elif players_first_card["value"] ==[1, 11] and players_second_card["value"] == [1, 11]:
             self.value_sum = 21
+
         else:
             self.value_sum = players_first_card["value"] + players_second_card["value"]
 
@@ -54,19 +63,26 @@ class Person:
             x = input("no card left, do you wanna continue? (y/n) ")
             if x == "y":
                 cards = Cards(name, kind).shuffle_deck()
+                #self.cardgenerator(cards, running, name, kind)
+                return cards
             else:
-                return running == False
+                cards = Cards(name, kind).shuffle_deck()
+                running = False
+                return running, cards
 
         return generated_card
 
     def card_value_manager(self, item):
         if item["value"] == [1, 11]:
+
             if self.value_sum <11:
                 self.value_sum += item["value"][1]
                 print(self.name, "cards value:", self.value_sum)
+
             else:
                 self.value_sum += item["value"][0]
                 print(self.name, "cards value:", self.value_sum)
+
         else:
             self.value_sum += item["value"]
             print(self.name, "cards value:", self.value_sum)
--- main.py
+++ main.py
@@ -7,17 +7,18 @@ name = [{"name": "A", "value": [1, 11]}, {"name": "2", "value": 2}, {"name": "3"
         , {"name": "5", "value": 5}, {"name": "6", "value": 6}, {"name": "7", "value": 7}, {"name": "8", "value": 8}
         , {"name": "9", "value": 9}, {"name": "10", "value": 10}, {"name": "jack", "value": 10}
         , {"name": "queen", "value": 10}, {"name": "king", "value": 10}]
+
 kind = ["Hearts", "Clubs", "Diamonds", "Spades"]
 
-# cards = list(range(1, 53))
 cards = Cards(name, kind).shuffle_deck()
-
 
 player = Person("player", "", 0, 0)
 dealer = Person("dealer", "", 0, 0)
 
 print("this is the game of blackjack")
+
 wanna_play = input("do you wanna proceed? (y/n): ")
+
 player.money = int(input("players credit: (ex. 500$ = 500) "))
 dealer.money = int(input("bank's credit: "))
 
@@ -26,16 +27,21 @@ running = True
 cards_value = 0
 
 while running:
+
     dealer.value_sum = 0
     dealer.cards_holding = ''
+
     player.value_sum = 0
     player.cards_holding = ''
+
     dealers_turn = False
+
     if wanna_play == "y":
         print("great lets start")
 
         dealer.dealer_first_turn_generator(cards, running, name, kind)
         player.player_first_turn_generator(cards, running, name, kind)
+
         print("cards count: " + str(len(cards)))
 
         bet = int(input("place your bet: "))
@@ -51,17 +57,21 @@ while running:
 
                 player_extra_card = player.cardgenerator(cards, running, name, kind)
                 print("cards count: " + str(len(cards)))
+
                 player.card_holding_manager(player_extra_card)
                 player.card_value_manager(player_extra_card)
+
                 if player.value_sum > 21:
                     dealer.money += bet
                     print("you lost", "balance: player(" + str(player.money) + ") bank(" + str(dealer.money) + ")")
                     break
+
                 elif player.value_sum == 21:
                     player.money += (2 * bet)
                     dealer.money -= bet
                     print("wow! you won!", "balance: player(" + str(player.money) + ") bank(" + str(dealer.money) + ")")
                     break
+
                 else:
                     x = input("you want an extra card? (y/n): ")
                     if x == "n":
@@ -103,6 +113,7 @@ while running:
                     dealer.money -= bet
                     print("you won!!", "balance: player(" + str(player.money) + ") bank(" + str(dealer.money) + ")")
             break
+
     elif wanna_play == "n":
         print("exiting game")
         break
